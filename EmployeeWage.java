670e48388d1d55a4f9cdd9d2cce111f6403c4b09
1301mohit <mohitkr1301@gmail.com>
2020-05-05 14:00:35 +0530

    ADDED: Part time wage in employee wage method

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,26 +1,30 @@
 
 class EmployeeWage {
 
-	public static int employeeWage() {
+	public static int employeeWage(int hour) {
 		int wagePerHour=20;
-      int fullDayHour=8;
-		return wagePerHour * fullDayHour;
+		return wagePerHour * hour;
 	}
 
    public static void employeeAttendance() {
-      int isFullTime=1;
-      int checkTime= (int)Math.floor(Math.random() * 10) % 2;
-      if(checkTime == isFullTime) {
-         int wage = employeeWage();
-			System.out.println("Employee wage: "+wage);
-		}
+      int fullTime=1;
+		int partTime=2;
+		int hour=0;
+      int checkTime= (int)Math.floor(Math.random() * 10) % 3;
+      if(checkTime == fullTime)
+			hour = 8;
+		else if(checkTime == partTime)
+			hour = 4;
       else
          System.out.println("Absent");
+		int wage = employeeWage(hour);
+      System.out.println("Employee wage: "+wage);
    }
 
    public static void main(String args[]) {
       System.out.println("Welcome to employee wage problem");
       employeeAttendance();
    }
+
 }
 
