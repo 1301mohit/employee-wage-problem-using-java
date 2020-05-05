3bb9febf3ff1f708e22a9ff6ea6783f014fbb70e
1301mohit <mohitkr1301@gmail.com>
2020-05-05 14:40:07 +0530

    ADDED: for loop for working day

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,22 +7,27 @@ class EmployeeWage {
 	}
 
    public static void employeeAttendance() {
-      final int FULL_TIME=1;
-		final int PART_TIME=2;
-		int hour=0;
-      int checkTime= (int)Math.floor(Math.random() * 10) % 3;
-      switch(checkTime) {
-			case FULL_TIME:
-				hour = 8;
-				break;
-			case PART_TIME:
-				hour = 4;
-				break;
-			default:
-				System.out.println("Absent");
+		int totalWage=0;
+		for(int day=1; day<=20; day++) {
+			final int FULL_TIME=1;
+      	final int PART_TIME=2;
+			int hour=0;
+			int checkTime= (int)Math.floor(Math.random() * 10) % 3;
+			System.out.println("Time: "+checkTime);
+			switch(checkTime) {
+         case FULL_TIME:
+            hour = 8;
+            break;
+         case PART_TIME:
+            hour = 4;
+            break;
+         default:
+            hour=0;
+      	}
+      	int wage = employeeWage(hour);
+			totalWage += wage; 
 		}
-		int wage = employeeWage(hour);
-      System.out.println("Employee wage: "+wage);
+      System.out.println("Employee wage: "+totalWage);
    }
 
    public static void main(String args[]) {
